09e2cbd531d7bb05fbaad37cec069c7845c63f0b
Binuri Karunarathna <154871134+BinuriKarunarathna@users.noreply.github.com>
2025-08-10 21:17:48 +0530

    Commited

--- src/services/api.js
+++ src/services/api.js
@@ -4,6 +4,9 @@ const API_BASE_URL = process.env.REACT_APP_API_URL ;
 const api = axios.create({
   baseURL: API_BASE_URL,
   withCredentials: true, // if you use cookies/auth
+  
 });
+
+console.log('API_BASE_URL:', API_BASE_URL);
 
 export default api;

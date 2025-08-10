7a324d1c521e5f4cec6ec2763c2455f273a203ee
Binuri Karunarathna <154871134+BinuriKarunarathna@users.noreply.github.com>
2025-08-10 13:40:09 +0530

    Use REACT_APP_API_URL from .env in UserService.js

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:3001/api/users";
+const API = process.env.REACT_APP_API_URL + "/users";
 
 export const registerUser = (userData) => axios.post(`${API}/register`, userData);
 export const loginUser = (loginData) => axios.post(`${API}/login`, loginData);
